09567560cc5bcffb7d033fcd2742b7d3b934cf7b
tompsay <urama.tomi@gmail.com>
2015-03-26 14:12:09 +0200

    createnews

--- application/config/routes.php
+++ application/config/routes.php
@@ -60,7 +60,8 @@ $route['default_controller'] = 'pages/view';
 $route['(:any)'] = 'pages/view/$1';
 */
 
-// TUTORIAL 2
+// TUTORIAL 2 & 3
+$route['news/create'] = 'news/create'; //3 
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
 $route['(:any)'] = 'pages/view/$1';
