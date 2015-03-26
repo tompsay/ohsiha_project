ef1d60f2096dfc6c6d8a5a20550548118cbf8f36
tompsay <urama.tomi@gmail.com>
2015-03-27 01:18:13 +0200

    delete

--- dbqueries/usersini.sql
+++ dbqueries/usersini.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
         id int(11) NOT NULL AUTO_INCREMENT,
         username varchar(128) NOT NULL,
         pwd varchar(128) NOT NULL,
-        PRIMARY KEY (id),
+        PRIMARY KEY (id)
 );
 
 INSERT users (username,pwd) VALUES('tomi', 'haukionkala');
