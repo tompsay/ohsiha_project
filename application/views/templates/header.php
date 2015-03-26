1edfe99ea456f13846e32d93b943ba182dabaf98
tompsay <urama.tomi@gmail.com>
2015-03-27 00:51:25 +0200

    delete

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,6 +6,8 @@
 
         <h1>CodeIgniter Tutorial</h1>
 		
-			<p><a href="/index.php/news">News</a></p>
-			<p><a href="/index.php/news/create">Create News</a></p>
-			<p><a href="/index.php/about">About</a></p>
+			<p>
+				<a href="/index.php/news">News</a>
+				<a href="/index.php/news/create">Create News</a>
+				<a href="/index.php/about">About</a>
+			</p>
