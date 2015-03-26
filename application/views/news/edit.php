7000c8da8133909a827e198d8197f1048ff0d796
tompsay <urama.tomi@gmail.com>
2015-03-27 00:47:53 +0200

    delete

--- application/views/news/edit.php
+++ application/views/news/edit.php
@@ -8,7 +8,7 @@
     <input type="input" name="title" value=" <?php echo $news_title ?> " /><br />
 
     <label for="text">Text</label>
-    <textarea name="text" value=" <?php echo $news_text ?> "></textarea><br />
+    <textarea name="text"> <?php echo $news_text ?> </textarea><br />
 
     <input type="submit" name="submit" value="Edit news item" />
 
