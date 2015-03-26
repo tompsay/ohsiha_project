8f00186a1fdb23205fbbcb7ab8ab60b6fd003316
tompsay <urama.tomi@gmail.com>
2015-03-26 19:46:08 +0200

    testing

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,5 +6,5 @@
 
         <h1>CodeIgniter Tutorial</h1>
 		
-			<p><a href="../news">News</a></p>
-			<p><a href="../pages/about">About</a></p>
+			<p><a href="index.php/news">News</a></p>
+			<p><a href="../index.php/about">About</a></p>
