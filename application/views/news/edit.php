184a2bd9302d6ab48ac26e5fea31004b628257f6
tompsay <urama.tomi@gmail.com>
2015-03-27 00:28:51 +0200

    delete

--- application/views/news/edit.php
+++ application/views/news/edit.php
@@ -5,10 +5,10 @@
 <?php echo form_open('news/edit') ?>
 
     <label for="title">Title</label>
-    <input type="input" name="title" value=" <?php echo $title ?> " /><br />
+    <input type="input" name="title" value=" <?php echo $editable_news['title'] ?> " /><br />
 
     <label for="text">Text</label>
-    <textarea name="text" value=" <?php echo $text ?> "></textarea><br />
+    <textarea name="text" value=" <?php echo $editable_news['text'] ?> "></textarea><br />
 
     <input type="submit" name="submit" value="Edit news item" />
 
