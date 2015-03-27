7ff08d04c071d29b70124d468eccd225ee402768
tompsay <urama.tomi@gmail.com>
2015-03-27 03:29:39 +0200

    delete

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,4 +1,6 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
 session_start(); //we need to call PHP's session object to access it through CI
 class Home extends CI_Controller {
 
