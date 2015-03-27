e420a129a245e27c72c7654973dc62d8dfcdba09
tompsay <urama.tomi@gmail.com>
2015-03-27 03:31:31 +0200

    delete

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -3,10 +3,10 @@ Class User extends CI_Model
 {
  function login($username, $password)
  {
-   $this -> db -> select('id, username, password');
+   $this -> db -> select('id, username, pwd');
    $this -> db -> from('users');
    $this -> db -> where('username', $username);
-   $this -> db -> where('password', $password);
+   $this -> db -> where('pwd', $password);
    $this -> db -> limit(1);
  
    $query = $this -> db -> get();
