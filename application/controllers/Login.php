f10734d4f51afb3aaa022d612d65c445082dff42
tompsay <urama.tomi@gmail.com>
2015-04-08 01:27:56 +0300

    id test

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -8,8 +8,9 @@ class Login extends CI_Controller {
 
  public function index()
  {
-   $this->load->helper('form');
-   $this->load->view('login_view');
+	$this->load->helper('url');
+	$this->load->helper('form');
+	$this->load->view('login_view');
  }
 
 }
