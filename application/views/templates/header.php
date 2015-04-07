1ae8e759ece88a5376431dfb7378ebc88589af79
tompsay <urama.tomi@gmail.com>
2015-04-08 02:58:13 +0300

    login test

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,6 +6,9 @@
 
         <h1>CodeIgniter Tutorial</h1>
 		
+		<h2>Welcome <?php echo $username; ?>!</h2>
+		<a href="home/logout">Logout</a>
+		
 			<p>
 				<a href="/index.php/news">News</a>
 				<a href="/index.php/news/create">Create News</a>
