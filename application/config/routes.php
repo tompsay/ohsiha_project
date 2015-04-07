f125962e9fb076481ded855526a4d0897742b45a
tompsay <urama.tomi@gmail.com>
2015-04-08 01:46:31 +0300

    login test

--- application/config/routes.php
+++ application/config/routes.php
@@ -61,10 +61,10 @@ $route['(:any)'] = 'pages/view/$1';
 */
 
 // TUTORIAL 2 & 3
-//$route['verifylogin'] = 'verifylogin';
+$route['verifylogin'] = 'verifylogin';
 $route['news/edit'] = 'news/edit';
 $route['news/create'] = 'news/create'; //3
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
-$route['(:any)'] = 'pages/view/$1';
+//$route['(:any)'] = 'pages/view/$1';
 $route['default_controller'] = 'login';
