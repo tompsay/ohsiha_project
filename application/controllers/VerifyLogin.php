76d47f68c2b02a2abbffce3c376bc449cfd35295
tompsay <urama.tomi@gmail.com>
2015-04-08 01:30:56 +0300

    id test

--- application/controllers/VerifyLogin.php
+++ application/controllers/VerifyLogin.php
@@ -1,61 +1,60 @@
 <?php 
 class Verifylogin extends CI_Controller {
 
- public function __construct()
- {
-	parent::__construct();
-	echo "validcon";
-	$this->load->model('user','',TRUE);
- }
+	public function __construct()
+	{
+		parent::__construct();
+		echo "validcon";
+		$this->load->model('user','',TRUE);
+	}
 
- public function index()
- {
-	echo "validindex";
-   //This method will have the credentials validation
-   $this->load->library('form_validation');
+	public function index()
+	{
+		echo "validindex";
+		//This method will have the credentials validation
+		$this->load->library('form_validation');
 
-   $this->form_validation->set_rules('username', 'Username', 'trim|required|xss_clean');
-   $this->form_validation->set_rules('password', 'Password', 'trim|required|xss_clean|callback_check_database');
+		$this->form_validation->set_rules('username', 'Username', 'trim|required|xss_clean');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required|xss_clean|callback_check_database');
 
-   if($this->form_validation->run() == FALSE)
-   {
-     //Field validation failed.  User redirected to login page
-     $this->load->view('login_view');
-   }
-   else
-   {
-     //Go to private area
-     redirect('home', 'refresh');
-   }
+		if($this->form_validation->run() == FALSE)
+		{
+			//Field validation failed.  User redirected to login page
+			$this->load->view('login_view');
+		}
+		else
+		{
+			//Go to private area
+			redirect('home', 'refresh');
+		}
 
- }
+	}
 
- public function check_database($password)
- {
-   //Field validation succeeded.  Validate against database
-   $username = $this->input->post('username');
+	public function check_database($password)
+	{
+		//Field validation succeeded.  Validate against database
+		$username = $this->input->post('username');
 
-   //query the database
-   $result = $this->user->login($username, $password);
+		//query the database
+		$result = $this->user->login($username, $password);
 
-   if($result)
-   {
-     $sess_array = array();
-     foreach($result as $row)
-     {
-       $sess_array = array(
-         'id' => $row->id,
-         'username' => $row->username
-       );
-       $this->session->set_userdata('logged_in', $sess_array);
-     }
-     return TRUE;
-   }
-   else
-   {
-     $this->form_validation->set_message('check_database', 'Invalid username or password');
-     return false;
-   }
- }
+		if($result)
+		{
+			$sess_array = array();
+			foreach($result as $row)
+			{
+				$sess_array = array(
+					'id' => $row->id,
+					'username' => $row->username
+					);
+				$this->session->set_userdata('logged_in', $sess_array);
+			}
+			return TRUE;
+		}
+		else
+		{
+			$this->form_validation->set_message('check_database', 'Invalid username or password');
+			return false;
+		}
+	}
 }
-?>
