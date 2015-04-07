130ba98fe092f9eb53f49e2c086cc84ea1dd0087
tompsay <urama.tomi@gmail.com>
2015-04-08 02:18:50 +0300

    login test

--- application/controllers/VerifyLogin.php
+++ application/controllers/VerifyLogin.php
@@ -5,7 +5,7 @@ class Verifylogin extends CI_Controller {
 	{
 		parent::__construct();
 		echo "validcon";
-		$this->load->model('user','',TRUE);
+		$this->load->model('user_model','',TRUE);
 	}
 
 	public function index()
@@ -17,7 +17,7 @@ class Verifylogin extends CI_Controller {
 		$this->form_validation->set_rules('username', 'Username', 'trim|required');
 		$this->form_validation->set_rules('password', 'Password', 'trim|required|callback_check_database');
 
-		if($this->form_validation->run() == FALSE)
+		if($this->form_validation->run() === FALSE)
 		{
 			//Field validation failed.  User redirected to login page
 			$this->load->view('login_view');
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,5 +1,5 @@
 <?php
-Class User extends CI_Model
+Class User_model extends CI_Model
 {
  public function __construct()
         {
