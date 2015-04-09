73231258b55dff38d9c1d68fb8d76d383721c952
tompsay <urama.tomi@gmail.com>
2015-04-09 17:14:01 +0300

    login test

--- application/views/news/index.php
+++ application/views/news/index.php
@@ -1,5 +1,3 @@
-<h2><?php echo $title ?></h2>
-
 <?php foreach ($news as $news_item): ?>
 
         <h3><?php echo $news_item['title'] ?></h3>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -9,8 +9,6 @@
 		<h2>Welcome <?php echo $_SESSION['logged_in']['username']; ?>!</h2>
 		<a href="login/logout">Logout</a>
 		
-		<?php echo var_dump($_SESSION); ?>
-		
 			<p>
 				<a href="/index.php/news">News</a>
 				<a href="/index.php/news/create">Create News</a>
