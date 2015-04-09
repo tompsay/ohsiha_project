c8ba6131d7a06313e5caea57cef19ce6b61f9aaa
tompsay <urama.tomi@gmail.com>
2015-04-09 23:32:36 +0300

    json test

--- application/views/news/index.php
+++ application/views/news/index.php
@@ -6,7 +6,7 @@
         </div>
         <p>
 			<a href="news/<?php echo $news_item['slug'] ?>">View article</a>
-			<a href="news/show_in_json">View article in JSON</a>
+			<a href="news/show_in_json/<?php echo $news_item['slug'] ?>">View article in JSON</a>
 			<?php echo anchor('news/edit/'.$news_item['id'], 'edit', array('class'=>'edit')); ?>
 			<?php echo anchor('news/delete/'.$news_item['id'], 'delete', array('class'=>'delete', 'onclick'=>"return confirm('Are you sure you want to delete this news item?')")); ?>
 		</p>
