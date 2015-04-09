47c658db2b5eb120324b563b73c1e12f7544280c
tompsay <urama.tomi@gmail.com>
2015-04-09 16:59:47 +0300

    login test

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -9,7 +9,7 @@
 		<h2>Welcome <?php echo $_SESSION['username']; ?>!</h2>
 		<a href="login/logout">Logout</a>
 		
-		<?php echo vardump($_SESSION); ?>
+		<?php echo var_dump($_SESSION); ?>
 		
 			<p>
 				<a href="/index.php/news">News</a>
