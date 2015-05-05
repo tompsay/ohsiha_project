14a0e1819b385ba7f460b5679fa38d233b36da96
tompsay <urama.tomi@gmail.com>
2015-05-06 00:58:02 +0300

    api testing

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -119,4 +119,14 @@ class News extends CI_Controller {
 			$this->load->view('news/json_view', $data);
 			$this->load->view('templates/footer');
 		}
+		
+		public function get_times_news()
+		{
+		
+			echo file_get_contents(http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1.json?api-key=cb5c634bda4ef7d5d97fbd397289f88b%3A9%3A72015177);
+				
+			$this->load->view('templates/header', $data);
+			$this->load->view('news/index', $data);
+			$this->load->view('templates/footer');
+		}
 }
